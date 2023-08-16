e4d5b4d783810b52b6f2cb63d95877defe95f4ab
AntonioVillegas13 <jevialan@gmail.com>
2023-08-16 08:02:44 -0500

    funciona 1era Funcion

---
+++ serviciosPuntoDeVenta.js
@@ -0,0 +1,7 @@
+calcularValorDescuento=function(monto,porcentajeDescuento){
+    return (monto*porcentajeDescuento)/100
+}
+
+calcularSubtotal=function(precio,cantidad){
+    return cantidad*precio
+}
